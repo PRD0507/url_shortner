041ceccfabc8d9be42dfa8924822100ddf27f70d
Prasad Desai <prdprasaddesai@gmail.com>
2023-07-27 17:23:48 +0530

    Minor changes

--- main/urls.py
+++ main/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('shorten', views.shorten_post, name='shorten_post'),
     path('shorten/<str:url>', views.shorten, name='shorten'),
+    path('load_url', views.load_url, name='shorten_post')
 ]
--- main/views.py
+++ main/views.py
@@ -1,19 +1,22 @@
-import random
-import string
-from django.utils import timezone
-
-from .models import LinkMapping
+from django.shortcuts import render
+from django.http import HttpResponse
+import pyshorteners
 
 
-def shorten(url):
-    random_hash = ''.join(random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for _ in range(7))
-    mapping = LinkMapping(original_url=url, hash=random_hash, creation_date=timezone.now())
-    mapping.save()
-    return random_hash
+# Create your views here.
+def shorten(request, url):
+    print("asdasdads")
+    shortener = pyshorteners.Shortener()
+    shortened_url = shortener.chilpit.short(url)
+    return HttpResponse(f'Shortened URL: <a href="{shortened_url}">{shortened_url}</a>')
+
+# def shorten(request, url):
+#     random_hash = ''.join(random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for _ in range(7))
+#     mapping = LinkMapping(original_url=url, hash=random_hash, creation_date=timezone.now())
+#     mapping.save()
+#     return random_hash
+
 
 
 def load_url(url_hash):
     return LinkMapping.objects.get(hash=url_hash)
-
-def shorten_post():
-    pass
